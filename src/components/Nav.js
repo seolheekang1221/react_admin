863f22ed90af792d3130915d38800bbe5a31c08d
ovdncids <ovdncids@nate.com>
2021-03-05 09:47:29 +0900

    React Router DOM

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
+import { NavLink } from 'react-router-dom';
+
 function Nav() {
   return (
     <nav className="nav">
       <ul>
-        <li><h2>Members</h2></li>
-        <li><h2>Search</h2></li>
+        <li><h2><NavLink to="members" exact={true} activeClassName='active'>Members</NavLink></h2></li>
+        <li><h2><NavLink to="search" exact={true} activeClassName='active'>Search</NavLink></h2></li>
       </ul>
     </nav>
   )
